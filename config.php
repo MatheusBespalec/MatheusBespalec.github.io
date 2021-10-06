51d63ce9b34350f941295aa7a3002a4f1a14c482
MatheusBespalec <81495141+MatheusBespalec@users.noreply.github.com>
2021-10-06 11:13:15 -0300

    volta

--- config.php
+++ config.php
@@ -4,19 +4,17 @@
 	date_default_timezone_set('America/Sao_Paulo');
 
 	//Links
-	define('INCLUDE_PATH', 'http://matheusbespalec.alwaysdata.net/');
+	define('INCLUDE_PATH', 'http://localhost/site_dinamico/');
 	define('INCLUDE_PATH_FULL', INCLUDE_PATH.'Views/');
 	define('INCLUDE_PATH_PAINEL', INCLUDE_PATH.'admin/painel/');
 	define('BASE_DIR',__DIR__.'/');
 
 	//DB
-	define('HOST','mysql-matheusbespalec.alwaysdata.net');
-	define('DB','matheusbespalec_matheus');
-	define('USER','243831');
-	define('PASS','Matheus1!');
+	define('HOST','localhost');
+	define('DB','portifolio');
+	define('USER','root');
+	define('PASS','');
 
-	//Autoload
-	
 	$autoload = function($class){
 		$class = str_replace('\\','/',$class);
 		if(file_exists(BASE_DIR.$class.'.php')){
